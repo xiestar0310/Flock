d821e34749922c5cd2a41f14b3ade9896e150bce
Rahma Gillan <rahmagillan2001@gmail.com>
2020-08-22 14:48:49 -0400

    Set your personal status

--- src/Room.js
+++ src/Room.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Video from "twilio-video";
 import Participant from "./Participant";
 import { getRoom, setFireRoom, getFireTime } from "./utils";
-import { Button, Card, Row, Col } from "react-bootstrap";
+import { Button, Card, Row, Col, Alert, Form } from "react-bootstrap";
 import "./Room.css";
 
 const Room = ({
@@ -23,7 +23,7 @@ const Room = ({
   const [min, setMin] = useState(null);
   const [sec, setSec] = useState(null);
   const [working, setWorking] = useState(true);
-    
+
   const startTimer = () => {
     setInterval(() => {
       if (beginTime) {
@@ -37,8 +37,16 @@ const Room = ({
     setHours(("0" + Math.floor(countTimer / 3600000)).slice(-2));
     setMin(("0" + (Math.floor(countTimer / 60000) % 60)).slice(-2));
     setSec(("0" + (Math.floor(countTimer / 1000) % 60)).slice(-2));
-    if (!!breakTime && !!workTime){
-        setWorking(!!((parseInt(hour)*60+parseInt(min)) % (parseInt(breakTime)+parseInt(workTime)) < workTime)?true:false); 
+    if (!!breakTime && !!workTime) {
+      setWorking(
+        !!(
+          (parseInt(hour) * 60 + parseInt(min)) %
+            (parseInt(breakTime) + parseInt(workTime)) <
+          workTime
+        )
+          ? true
+          : false
+      );
     }
   }, [countTimer]);
 
@@ -58,22 +66,21 @@ const Room = ({
     };
 
     Video.connect(token, {
-      name: roomName
-    }).then(async(room) => {
-      const rr = await getRoom({"sid":room.sid});
-      if (workTime && breakTime){
-          await setFireRoom({
-              "sid": room.sid, 
-              "workTime": workTime, 
-              "breakTime": breakTime,
-          }
-          );
-      }else{
-          const times = await getFireTime({"sid":room.sid});
-          if (times.data){
-              setBreakTime(times.data.restTime);
-              setWorkTime(times.data.workTime);
-          }
+      name: roomName,
+    }).then(async (room) => {
+      const rr = await getRoom({ sid: room.sid });
+      if (workTime && breakTime) {
+        await setFireRoom({
+          sid: room.sid,
+          workTime: workTime,
+          breakTime: breakTime,
+        });
+      } else {
+        const times = await getFireTime({ sid: room.sid });
+        if (times.data) {
+          setBreakTime(times.data.restTime);
+          setWorkTime(times.data.workTime);
+        }
       }
       if (rr.status === 200) {
         setRoomDetails(rr.data);
@@ -104,42 +111,65 @@ const Room = ({
   }, [roomName, token]);
 
   const remoteParticipants = participants.map((participant) => (
-    <Participant key={participant.sid} participant={participant} />
+    <Participant
+      key={participant.sid}
+      participant={participant}
+      remote={true}
+    />
   ));
 
   return (
     <div className="room">
       <Card className="localParticipantContainer">
         <Row>
-          <Col className="text-left">
+          <Col className="text-left" md={5}>
             <h5>
               Hi {room ? room.localParticipant.identity : null}, welcome to your
               Flock room!
             </h5>
+            <div className="smSeperator" />
             <h5>Room: {roomName}</h5>
             <p>Invite others by telling them your room name</p>
+            <div className="smSeperator" />
             <h5>
               Total Session Time: {beginTime ? hour + ":" + min + ":" + sec : 0}
             </h5>
-            <h5>{!!workTime && !!breakTime ? ("Work: " + workTime + "mins") + ("Break: " + breakTime + "mins"): null}</h5>
-            <h5>{working ? "Work" : "Break"}</h5>
-            <br />
-            <Button className="logoutBtn" onClick={handleLogout}>
-              Log Out
-            </Button>
+            <p>
+              {!!workTime && !!breakTime
+                ? "Work: " +
+                  workTime +
+                  " mins" +
+                  (" Break: " + breakTime + " mins")
+                : null}
+            </p>
+            <h5>
+              {working ? (
+                <Alert className="workIndicator" variant="success">
+                  Work
+                </Alert>
+              ) : (
+                <Alert className="workIndicator" variant="danger">
+                  Break
+                </Alert>
+              )}
+            </h5>
           </Col>
-          <Col>
+          <Col md={7}>
             <div className="local-participant">
               {room ? (
                 <Participant
                   key={room.localParticipant.sid}
                   participant={room.localParticipant}
+                  remote={false}
                 />
               ) : (
                 ""
               )}
             </div>
           </Col>
+          <Button className="logoutBtn" onClick={handleLogout}>
+            Log Out
+          </Button>
         </Row>
       </Card>
       <h3>Remote Participants</h3>
